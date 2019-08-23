2bb07489346b12b2cf2d32b34eb7acbd2b1af2a3
Evan Farr <roboproductions200@gmail.com>
2019-08-23 12:48:31 -0600

    Initial commit

---
+++ Chapter9/Quadratic.java
@@ -0,0 +1,18 @@
+public class Quadratic
+{
+    public static void main (String [] args)
+    {
+        double x = 0.0;
+        double x2 = 2.0;
+        double x3 = 4.0;
+        double answer = 0;
+        double answer2 = 0;
+        double answer3 = 0;
+        answer  = (3 * (x * x)) -8 * x + 4;
+        answer2  = (3 * (x2 * x2)) -8 * x2 + 4;
+        answer3  = (3 * (x3 * x3)) -8 * x3 + 4;
+         System.out.println("Answer: " + answer);
+         System.out.println("Answer: " + answer2);
+         System.out.println("Answer: " + answer3);
+    }
+}
