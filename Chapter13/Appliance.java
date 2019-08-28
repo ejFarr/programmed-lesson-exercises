08ed2cde9ec1798940cf73db8e710d23a1044d90
Evan Farr <roboproductions200@gmail.com>
2019-08-28 12:31:04 -0600

    Appliance exercise

---
+++ Chapter13/Appliance.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Appliance
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Enter cost per kilowatt-hour in cents");
+        Scanner scim = new Scanner(System.in);
+
+        System.out.println("Enter kilowatt-hours used per year");
+        Scanner scram = new Scanner(System.in);
+
+        double cost = scim.nextDouble() * scram.nextDouble();
+        System.out.println("Annual cost: " + cost);
+
+    }
+}
