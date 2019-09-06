469f47349c67c2800d54ccf74da0a2bee197198d
Evan Farr <roboproductions200@gmail.com>
2019-09-06 12:39:57 -0600

    Chap 16 2

---
+++ Chapter16/OrderCheck.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+public class OrderCheck
+{
+    public static void main(String[] args)
+    {
+        Scanner scram = new Scanner(System.in);
+        int bolt;
+        int nut;
+        int wash;
+        boolean n;
+        boolean w;
+        int cost;
+        System.out.println("Number of bolts: ");
+        bolt = scram.nextInt();
+        System.out.println("Number of nuts: ");
+        nut = scram.nextInt();
+        System.out.println("Number of washers: ");
+        wash = scram.nextInt();
+
+        cost = (bolt * 5) + (nut * 3) + (wash * 1);
+        if(nut >= bolt)
+        {
+            n = true;
+        }
+        else
+        {
+            n = false;
+        }
+        if (wash >= (2 * bolt))
+        {
+            w = true;
+        }
+        else
+        {
+            w = false;
+        }
+        if (n == false)
+        {
+            System.out.println("Check the order: too few nuts");
+        }
+        else if (n)
+        {
+            System.out.println("Check the order: Enough nuts");
+        }
+        if (w == false)
+        {
+            System.out.println("Check the order: too few washers");
+        }
+        else if (w)
+        {
+            System.out.println("Check the order: Enough washers");
+        }
+
+        System.out.println("Total cost: " + cost);
+    }
+}
