8739b260f9bf1c76ebb3856011c691dbbaa9968a
Evan Farr <roboproductions200@gmail.com>
2019-09-10 12:15:21 -0600

    C17 1

--- Chapter17/Internet.java
+++ Chapter17/Internet.java
@@ -3,6 +3,33 @@ public class Internet
 {
     public static void main (String[] args)
     {
-        
+        Scanner scram = new Scanner(System.in);
+        System.out.println("Enter the item: ");
+        String item = scram.nextLine();
+
+        System.out.println("Enter the price: ");
+        double price = scram.nextDouble();
+
+        System.out.println("Overnight delivery (0==no, 1==yes): ");
+        int night = scram.nextInt();
+
+        double deliv = 0.00;
+        double finale;
+
+        if (price < 10.00)
+        {
+            deliv = 2.00;
+        }
+        if (night == 1)
+        {
+            deliv += 3.00;
+        }
+        finale = price + deliv;
+
+        System.out.println("Invoice: ");
+        System.out.println(     item + " " + price);
+        System.out.println(     "delivery " + deliv);
+        System.out.println(     "total " + finale);
+
     }
 }
