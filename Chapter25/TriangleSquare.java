6a080b7f8ed3290ccd4d58ed85e6308b2455fe3e
Evan Farr <roboproductions200@gmail.com>
2019-09-24 19:18:08 -0600

    Completes Chapter 25

--- Chapter25/TriangleSquare.java
+++ Chapter25/TriangleSquare.java
@@ -1,9 +1,28 @@
 import java.util.Scanner;
 public class TriangleSquare
 {
-    public static void amin (String[] args)
+    public static void main (String[] args)
     {
-        Scanner scan = new Sacnner(System.in);
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Insert limit: ");
+        int limit = scan.nextInt();
+        int sum = 0;
+        int square = 0;
+
+        for (int i = 1; i <= limit; i++)
+        {
+            
+            square = i * i;
+
+            for (int j = 1; j <= limit; j++)
+            {
+                sum = (j * j + j) / 2;
+                if (sum == square)
+            {
+                System.out.println(square + " is both a square and triangle number.");
+            }
+            }
+            
+        }
     }
 }
