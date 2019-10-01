29465ccd3b7ae6e3c3c7c56d804052b85cf4baef
Evan Farr <roboproductions200@gmail.com>
2019-10-01 12:11:09 -0600

    Chapter 30 guessing game

--- Chapter30/Guessing.java
+++ Chapter30/Guessing.java
@@ -7,16 +7,27 @@ public class Guessing
         Scanner scan = new Scanner(System.in);
         int number = rand.nextInt(10) + 1;
         System.out.println("I am thinkning of a number from 1 to 10. You must guess what it is in three tries.");
-        System.out.println("Enter a guess: ");
-        int guess = scan.nextInt();
+        for (int i = 1; i <= 3; i ++)
+        {
+            System.out.println("Enter a guess: ");
+            int guess = scan.nextInt();
 
-        if (guess == number)
-        {
-            System.out.println("Right");
-        }
-        else
-        {
-            System.out.println("Wrong.");
+            if (guess == number)
+            {
+                System.out.println("Right");
+                System.out.println("You have won the game.");
+                break;
+            }
+            else if (i == 3)
+            {
+                System.out.println("Wrong.");
+                System.out.println("The correct number was " + number);
+                System.out.println("You have lost the game.");
+            }
+            else
+            {
+                System.out.println("Wrong.");
+            }
         }
     }
 }
