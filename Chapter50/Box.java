b8d2f25681c6b5c8bf04b3a0e761b7a708e65fc7
Evan Farr <roboproductions200@gmail.com>
2019-11-01 12:53:31 -0600

    more box add

--- Chapter50/Box.java
+++ Chapter50/Box.java
@@ -9,13 +9,43 @@ public class Box {
         this.length = length;
     }
     public Box(double side) {
-
+        width = side;
+        height = side;
+        length = side;
+    }
+    public Box(Box oldBox) {
+        this.width = oldBox.width;
+        this.height = oldBox.height;
+        this.length = oldBox.length;
+    }
+    public Box biggerBox(Box oldBox) {
+        return new Box (1.25 * oldBox.width(),1.25 * oldBox.length(), 1.25 * oldBox.height());
+    }
+    public double length() {
+        return length;
+    }
+    public double width() {
+        return width;
+    }
+    public double height() {
+        return height;
+    }
+    private double topArea() {
+        return width * length;
+    }
+    private double faceArea() {
+        return height * length;
+    }
+    private double sideArea() {
+        return height * width;
     }
     public double volume() {
         return height * width * length;
     }
     public double area() {
-        return 2 * ((height * width) + (height * length) + (length * width));
+        return 2 * (topArea() + faceArea() + sideArea());
     }
-    
+    public String toString() {
+        return "topArea: " + topArea();
+    }
 }
--- Chapter50/TestBox.java
+++ Chapter50/TestBox.java
@@ -1,5 +1,17 @@
 public class TestBox {
-    public static void main(String[] args) {
-        
-    }
+
+  public static void main ( String[] args ) {
+
+    Box box1 = new  Box( 2.5, 3.0, 5.0 );
+
+    System.out.println( "Box1 Area: "    + box1.area() + 
+                        "  Volume: " + box1.volume()  );
+
+    Box box2 = new Box ( 3.0 );
+
+    System.out.println( "Box2 Area: "    + box2.area() + 
+                        "  Volume: " + box2.volume()  );
+
+
+  }
 }
