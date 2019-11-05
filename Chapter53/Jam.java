24917d8d83d0e3e428662779edb310d9b75700b2
Evan Farr <roboproductions200@gmail.com>
2019-11-05 12:13:16 -0700

    c53 jam, pantry, tester

--- Chapter53/Jam.java
+++ Chapter53/Jam.java
@@ -1,3 +1,29 @@
 public class Jam {
-    
+    private String contents;
+    private String date;
+    private int capacity;
+
+    public Jam (String contents, String date, int size) {
+        this . contents = contents  ;
+        this . date = date ;
+        capacity = size;
+    }
+    public boolean empty() {
+        return (capacity == 0);
+    }
+    public String toString() {
+        return contents + "   " +  date + "   " +  capacity + " fl. oz."  ;
+    }
+    public void spread ( int fluidOz ) {
+    if ( !empty() ) {
+      if ( fluidOz <= capacity ) {
+        System.out.println("Spreading " + fluidOz + " fluid ounces of " + contents );
+        capacity = capacity - fluidOz ;
+      } else {
+        System.out.println("Spreading " + capacity + " fluid ounces of " + contents );
+        capacity =  0 ;
+      }
+     } else
+       System.out.println("No jam in the Jar!");
+  }
 }
