2727d00ed0b53807be244d2ab2c682ab7148cf28
Evan Farr <roboproductions200@gmail.com>
2019-11-06 13:02:47 -0700

    Finishes exercise 2

--- Chapter53/Pantry.java
+++ Chapter53/Pantry.java
@@ -6,32 +6,60 @@ public class Pantry {
   private Jam  selected ;
 
   // Constructors
-  Pantry( Jam jar1, Jam jar2, Jam jar3 ) {
-    this . jar1 = jar1 ;
-    this . jar2 = jar2 ;
-    this . jar3 = jar3 ;
-    selected = null ;
+  public Pantry( Jam jar1, Jam jar2, Jam jar3 ) {
+    this.jar1 = jar1;
+    this.jar2 = jar2;
+    this.jar3 = jar3;
+    selected = null;
+  }
+
+  public Pantry (Jam jar1, Jam jar2) {
+    this.jar1 = jar1;
+    this.jar2 = jar2;
+    this.jar3 = null;
+    selected = null;
+  }
+  public Pantry (Jam jar1) {
+    this.jar1 = jar1;
+    this.jar2 = null;
+    this.jar3 = null;
+    selected = null;
   }
 
   // Methods
   public String toString() {
     String str = "";
     str += "1: " +  jar1.toString()  + "\n";
-    str += "2: " +  jar2.toString()  + "\n";
-    str += "3: " +  jar3.toString()  + "\n";     
+    if (jar2 != null)
+      str += "2: " +  jar2.toString()  + "\n";
+
+    if (jar3 != null)
+      str += "3: " +  jar3.toString()  + "\n";     
     return str;
   }
 
+  public void print() {
+    System.out.println(this.toString());
+  }
+
   // assume that the user entered a correct selection, 1, 2, or 3
-  public void select( int jarNumber ) {
-    if ( jarNumber == 1 )
+  public boolean select( int jarNumber ) {
+    if ( jarNumber == 1 && jar1 != null) {
       selected =  jar1 ;
+      return true;
+    }
 
-    else if ( jarNumber == 2 )
+    if ( jarNumber == 2 && jar2 != null) {
       selected = jar2 ;
+      return true;
+    }
 
-    else 
+    if ( jarNumber == 3 && jar3 != null) {
       selected = jar3 ;
+      return true;
+    }
+    else 
+      return false;
   }
 
   // spread the selected jam
