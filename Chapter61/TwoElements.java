3dee9ba70f67a3c3f71681a1e355e1010473556f
Evan Farr <roboproductions200@gmail.com>
2019-12-03 12:53:27 -0700

    Starts exercise 2

---
+++ Chapter61/TwoElements.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class TwoElements
+{
+
+  public static void main ( String[] args )  {
+    int[] data = {3, 1, 5, 7, 4, 12, -3, 8, -2};
+    
+    // declare and initialize variables for the two largest
+    int large1 = 0;
+    int large2 = 0;
+    
+    // compute the two largest
+    for ( int index = 0; index < data.length; index++) {
+        
+    }
+      
+    // write out the two largest
+    System.out.println(  );
+
+  }
+}
