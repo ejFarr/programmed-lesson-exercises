18dfa4c793550d0d0730ef4b931e3bab7fd6773d
Evan Farr <roboproductions200@gmail.com>
2019-12-04 11:46:49 -0700

    c61 exercise 2

--- Chapter61/TwoElements.java
+++ Chapter61/TwoElements.java
@@ -7,16 +7,22 @@ public class TwoElements
     int[] data = {3, 1, 5, 7, 4, 12, -3, 8, -2};
     
     // declare and initialize variables for the two largest
-    int large1 = 0;
+    int large1 = data[0];
     int large2 = 0;
     
     // compute the two largest
     for ( int index = 0; index < data.length; index++) {
-        
+        if (data[index] > large1) {
+          large2 = large1;
+          large1 = data[index];
+        }
+        if (data[index] < large1 && data[index] > large2) {
+          large2 = data[index];
+        }
     }
       
     // write out the two largest
-    System.out.println(  );
+    System.out.println("The two largest are: "  + large1 + " " + large2);
 
   }
 }
