b73a7943835f4449fd33a0d4888390ec5dd03a35
Evan Farr <roboproductions200@gmail.com>
2019-12-04 13:10:11 -0700

    Starts c61 exercise 5

---
+++ Chapter61/Panagram.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Panagram {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] count = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        
+        System.out.println("Enter your sentence: ");
+        String panagram = scan.next();
+        String alphabet = "abcdefghijklnmopqrstuvwxyz";
+        for (int j = 0; j < panagram.length(); j++) {
+            for (int i = 0; i < alphabet.length(); i++) {
+
+            }
+        }
+    }
+}
