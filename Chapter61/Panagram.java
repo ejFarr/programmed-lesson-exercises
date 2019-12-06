3f9b2c3e33b44fa10cde5621b866f96e35f8d75c
Evan Farr <roboproductions200@gmail.com>
2019-12-06 12:42:01 -0700

    completes c61 exercise 5

--- Chapter61/Panagram.java
+++ Chapter61/Panagram.java
@@ -4,15 +4,29 @@ public class Panagram {
     public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int[] count = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-        
+        int[] count = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        boolean isPanagram = false;
         System.out.println("Enter your sentence: ");
-        String panagram = scan.next();
+        String panagram = scan.nextLine().toLowerCase();
+        
+
         String alphabet = "abcdefghijklnmopqrstuvwxyz";
         for (int j = 0; j < panagram.length(); j++) {
             for (int i = 0; i < alphabet.length(); i++) {
-
+                if (panagram.charAt(j) == alphabet.charAt(i)) {
+                    count[i] += 1;
+                    System.out.println(panagram.charAt(j));
+                }
             }
         }
+        for (int i = 0; i < count.length; i ++) {
+            if (count[i] > 0) {
+                isPanagram = true;
+            }
+            else { 
+                isPanagram = false;
+            }
+        }
+        System.out.println(isPanagram);
     }
 }
